6f8db9d119b2a13a0d14cf68fcee0d0a1df4f29f
FrankZiWANG-dev <wang.frank.zi@gmail.com>
2021-11-03 17:13:06 +0100

    mobile first but missing techs and contact

--- src/assets/components/Intro.js
+++ src/assets/components/Intro.js
@@ -4,27 +4,29 @@ import "../css/output.css";
 const Intro = () => {
     return (
         <div style={{backgroundImage: "url('https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/desk.jpg?raw=true')"}}
-        class="h-3/5 w-screen   p-5
+        class="h-4/6 w-screen p-5
         flex justify-center items-center
         bg-fixed bg-cover">
-            <div class="flex  
+            <div class="flex
             rounded-2xl shadow-sm
             bg-white bg-opacity-40
             backdrop-filter backdrop-blur-sm drop-shadow">
-                <div class="ml-5 mr-3 self-center">
-                    <img class="rounded-full border-2 border-yellow-400" src="https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Photo%20CV.jpg?raw=true" alt="cv"/>
+                <div style={{backgroundImage: "url('https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Photo%20CV.jpg?raw=true')"}}
+                class="ml-3 w-full h-48 self-center
+                bg-cover bg-center
+                rounded-full border-2 border-yellow-400">
                 </div>
                 <div class="self-center
                 flex flex-col
-                p-5 ml-3 mr-5">
-                    <div class="self-end rounded-xl w-3/5 bg-gray-100 bg-opacity-70 p-2">
+                p-5 ml-1 mr-5">
+                    {/* <div class="self-end rounded-xl w-3/5 bg-gray-100 bg-opacity-70 p-2">
                         <p class="italic text-left">"Well done is better than well said" </p>
                         <p class="italic text-right"> Benjamin Franklin.</p>
-                    </div>
+                    </div> */}
                 
                     <p class="font-semibold"> Welcome to my online CV! </p>
-                    <p> Take a look at my academic background, technological preferences, projects and interests.
-                        Feel free to fill out the form at the bottom of the page to share your opinion or a professional opportunity.</p>
+                    <p> Take a look at my academic background, technological preferences, projects and interests.</p>
+                    <p>Feel free to fill out the form at the bottom of the page to share your opinion or a professional opportunity.</p>
                 </div>
             </div>
         </div>
--- src/assets/components/Projects.js
+++ src/assets/components/Projects.js
@@ -11,18 +11,19 @@ const Projects = () => {
     };
 
     return (
-        <div class="w-4/5 ml-auto mr-auto flex-col items-center ">
+        <div class="w-4/5 ml-auto mr-auto flex-col items-center">
            <p class="text-center text-gray-100 text-xl leading-normal font-semibold"> Projects </p>
            <Slider {...settings}>
                 <div class="rounded-3xl flex items-center justify-center">
                     <a class='rounded' href="https://github.com/FrankZiWANG-dev/Guinea-pig-presidentials" target="_blank">
                         <figure class="relative">
-                            <img class="pl-8 pt-5 pr-8" src='https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Guinea-pig-presidentials-screenshot.png?raw=true'/>
-                            <figcaption class="absolute -mt-32 right-0 mr-12 p-5
+                            <img class="pl-3 pr-3 pt-5 md:pl-8 md:pr-8" src='https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Guinea-pig-presidentials-screenshot.png?raw=true'/>
+                            <figcaption class=" mt-5 mr-5 ml-5 p-3 text-sm
+                                        md:absolute md:-mt-32 md:right-0 md:mr-12 md:p-5
                                         rounded-xl shadow-sm
-                                        bg-white bg-opacity-40
-                                        backdrop-filter backdrop-blur-sm drop-shadow">
-                                <p class="font-semibold text-lg">Guinea pig presidentials</p>
+                                        bg-white bg-opacity-90
+                                        md:bg-opacity-40 md:backdrop-filter md:backdrop-blur-sm md:drop-shadow">
+                                <p class="font-semibold md:text-lg">Guinea pig presidentials</p>
                                 <p> Meet my guinea pigs and elect your favorite!</p>
                                 <p> Frontend in HTML / Sass, Backend in PHP. </p>
                             </figcaption>
@@ -32,12 +33,13 @@ const Projects = () => {
                 <div class="rounded-3xl flex items-center justify-center">
                     <a class='rounded' href="https://github.com/FrankZiWANG-dev/Guinea-pig-presidentials" target="_blank">
                         <figure class="relative">
-                            <img class="pl-8 pt-5 pr-8" src='https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Guinea-pig-presidentials-screenshot.png?raw=true'/>
-                            <figcaption class="absolute -mt-32 right-0 mr-12 p-5
+                            <img class="pl-3 pr-3 pt-5 md:pl-8 md:pr-8" src='https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Guinea-pig-presidentials-screenshot.png?raw=true'/>
+                            <figcaption class=" mt-5 mr-5 ml-5 p-3 text-sm
+                                        md:absolute md:-mt-32 md:right-0 md:mr-12 md:p-5
                                         rounded-xl shadow-sm
-                                        bg-white bg-opacity-40
-                                        backdrop-filter backdrop-blur-sm drop-shadow">
-                                <p class="font-semibold text-lg">Gaming-themed Cookie Clicker</p>
+                                        bg-white bg-opacity-90
+                                        md:bg-opacity-40 md:backdrop-filter md:backdrop-blur-sm md:drop-shadow">
+                                <p class="font-semibold md:text-lg">Gaming-themed Cookie Clicker</p>
                                 <p> Click the icon to get points, spend to get bonuses. Repeat.</p>
                                 <p> Frontend in HTML / Sass / Javascript. No backend. </p>
                             </figcaption>
@@ -47,12 +49,13 @@ const Projects = () => {
                 <div class="rounded-3xl flex items-center justify-center">
                     <a class='rounded' href="https://github.com/FrankZiWANG-dev/Guinea-pig-presidentials" target="_blank">
                         <figure class="relative">
-                            <img class="pl-8 pt-5 pr-8" src='https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Guinea-pig-presidentials-screenshot.png?raw=true'/>
-                            <figcaption class="absolute -mt-32 right-0 mr-12 p-5
+                            <img class="pl-3 pr-3 pt-5 md:pl-8 md:pr-8" src='https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Guinea-pig-presidentials-screenshot.png?raw=true'/>
+                            <figcaption class=" mt-5 mr-5 ml-5 p-3 text-sm
+                                        md:absolute md:-mt-32 md:right-0 md:mr-12 md:p-5
                                         rounded-xl shadow-sm
-                                        bg-white bg-opacity-40
-                                        backdrop-filter backdrop-blur-sm drop-shadow">
-                                <p class="font-semibold text-lg">Pet Database</p>
+                                        bg-white bg-opacity-90
+                                        md:bg-opacity-40 md:backdrop-filter md:backdrop-blur-sm md:drop-shadow">
+                                <p class="font-semibold md:text-lg">Pet Database</p>
                                 <p> My most ambitious project: a pet related social network!</p>
                                 <p> Frontend in React / Tailwind / Sass, Backend in NodeJS / MongoDB. </p>
                             </figcaption>
@@ -62,12 +65,13 @@ const Projects = () => {
                 <div class="rounded-3xl flex items-center justify-center">
                     <a class='rounded' href="https://github.com/FrankZiWANG-dev/Guinea-pig-presidentials" target="_blank">
                         <figure class="relative">
-                            <img class="pl-8 pt-5 pr-8" src='https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Guinea-pig-presidentials-screenshot.png?raw=true'/>
-                            <figcaption class="absolute -mt-32 right-0 mr-12 p-5
+                            <img class="pl-3 pr-3 pt-5 md:pl-8 md:pr-8" src='https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Guinea-pig-presidentials-screenshot.png?raw=true'/>
+                            <figcaption class=" mt-5 mr-5 ml-5 p-3 text-sm
+                                        md:absolute md:-mt-32 md:right-0 md:mr-12 md:p-5
                                         rounded-xl shadow-sm
-                                        bg-white bg-opacity-40
-                                        backdrop-filter backdrop-blur-sm drop-shadow">
-                                <p class="font-semibold text-lg">Client project for Becode</p>
+                                        bg-white bg-opacity-90
+                                        md:bg-opacity-40 md:backdrop-filter md:backdrop-blur-sm md:drop-shadow">
+                                <p class="font-semibold md:text-lg">Client project for Becode</p>
                                 <p> Becode needed a platform to centralize client projects done by their students.</p>
                                 <p> Technologies to be determined yet. </p>
                             </figcaption>
