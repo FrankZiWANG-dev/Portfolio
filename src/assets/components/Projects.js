62d98c9cc9a1b8abb6ef6149d9fa2558ebff29e4
FrankZiWANG-dev <wang.frank.zi@gmail.com>
2021-11-03 12:36:52 +0100

    carousel fixed, links and images not up to date

--- src/assets/components/Projects.js
+++ src/assets/components/Projects.js
@@ -11,32 +11,67 @@ const Projects = () => {
     };
 
     return (
-        <div class="text-center w-screen">
-           <p class="text-gray-100 text-xl leading-normal font-semibold"> Projects </p>
+        <div class="w-4/5 ml-auto mr-auto flex-col items-center ">
+           <p class="text-center text-gray-100 text-xl leading-normal font-semibold"> Projects </p>
            <Slider {...settings}>
-               <div class="bg-gray-400 rounded-3xl flex flex-col justify-center">
-                    <a href="http://www.github.com/" target="_blank"
-                    class="w-1/5 h-1/5"
-                    style={{backgroundImage: "url('https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Photo%20CV.jpg?raw=true')"}}>
-                        <div> Text <br/>Text <br/>Text <br/>Text <br/></div>
+                <div class="rounded-3xl flex items-center justify-center">
+                    <a class='rounded' href="https://github.com/FrankZiWANG-dev/Guinea-pig-presidentials" target="_blank">
+                        <figure class="relative">
+                            <img class="pl-8 pt-5 pr-8" src='https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Guinea-pig-presidentials-screenshot.png?raw=true'/>
+                            <figcaption class="absolute -mt-32 right-0 mr-12 p-5
+                                        rounded-xl shadow-sm
+                                        bg-white bg-opacity-40
+                                        backdrop-filter backdrop-blur-sm drop-shadow">
+                                <p class="font-semibold text-lg">Guinea pig presidentials</p>
+                                <p> Meet my guinea pigs and elect your favorite!</p>
+                                <p> Frontend in HTML / Sass, Backend in PHP. </p>
+                            </figcaption>
+                        </figure>
                     </a>
                 </div>
-                <div>
-                    <a href="http://www.github.com/" target="_blank">
-                        <img src="https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Photo%20CV.jpg?raw=true"/>
-                        <div> Text </div>
+                <div class="rounded-3xl flex items-center justify-center">
+                    <a class='rounded' href="https://github.com/FrankZiWANG-dev/Guinea-pig-presidentials" target="_blank">
+                        <figure class="relative">
+                            <img class="pl-8 pt-5 pr-8" src='https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Guinea-pig-presidentials-screenshot.png?raw=true'/>
+                            <figcaption class="absolute -mt-32 right-0 mr-12 p-5
+                                        rounded-xl shadow-sm
+                                        bg-white bg-opacity-40
+                                        backdrop-filter backdrop-blur-sm drop-shadow">
+                                <p class="font-semibold text-lg">Gaming-themed Cookie Clicker</p>
+                                <p> Click the icon to get points, spend to get bonuses. Repeat.</p>
+                                <p> Frontend in HTML / Sass / Javascript. No backend. </p>
+                            </figcaption>
+                        </figure>
                     </a>
                 </div>
-                <div>
-                    <a href="http://www.github.com/" target="_blank">
-                        <img src="https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Photo%20CV.jpg?raw=true"/>
-                        <div> Text </div>
+                <div class="rounded-3xl flex items-center justify-center">
+                    <a class='rounded' href="https://github.com/FrankZiWANG-dev/Guinea-pig-presidentials" target="_blank">
+                        <figure class="relative">
+                            <img class="pl-8 pt-5 pr-8" src='https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Guinea-pig-presidentials-screenshot.png?raw=true'/>
+                            <figcaption class="absolute -mt-32 right-0 mr-12 p-5
+                                        rounded-xl shadow-sm
+                                        bg-white bg-opacity-40
+                                        backdrop-filter backdrop-blur-sm drop-shadow">
+                                <p class="font-semibold text-lg">Pet Database</p>
+                                <p> My most ambitious project: a pet related social network!</p>
+                                <p> Frontend in React / Tailwind / Sass, Backend in NodeJS / MongoDB. </p>
+                            </figcaption>
+                        </figure>
                     </a>
                 </div>
-                <div>
-                    <a href="http://www.github.com/" target="_blank">
-                        <img src="https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Photo%20CV.jpg?raw=true"/>
-                        <div> Text </div>
+                <div class="rounded-3xl flex items-center justify-center">
+                    <a class='rounded' href="https://github.com/FrankZiWANG-dev/Guinea-pig-presidentials" target="_blank">
+                        <figure class="relative">
+                            <img class="pl-8 pt-5 pr-8" src='https://github.com/FrankZiWANG-dev/Portfolio/blob/main/src/assets/images/Guinea-pig-presidentials-screenshot.png?raw=true'/>
+                            <figcaption class="absolute -mt-32 right-0 mr-12 p-5
+                                        rounded-xl shadow-sm
+                                        bg-white bg-opacity-40
+                                        backdrop-filter backdrop-blur-sm drop-shadow">
+                                <p class="font-semibold text-lg">Client project for Becode</p>
+                                <p> Becode needed a platform to centralize client projects done by their students.</p>
+                                <p> Technologies to be determined yet. </p>
+                            </figcaption>
+                        </figure>
                     </a>
                 </div>
             </Slider>
